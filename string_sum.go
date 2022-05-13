d832c30c53afb66485d1ef0c5c2eb49dc723928a
vital <vitalikgk@mail.ru>
2022-05-13 22:59:53 +0400

    test

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,10 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +27,50 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	//input := " 5 + 3 +6 -2    "
+
+	//fmt.Println("Test string", x)
+
+	input = strings.ReplaceAll(input, " ", "")
+	//fmt.Println("x=", x)
+	re := regexp.MustCompile(`[\\+\\-]*[0-9]+`)
+	//fmt.Println(re.FindAllString(x, -1))
+	n := re.FindAllString(input, -1)
+	//fmt.Printf("Type=%T?, Len=%d", n, len(n))
+	if len(n) == 0 {
+		err := fmt.Errorf("\n Ошибка пустое значение: %w", errorEmptyInput)
+		fmt.Println(err.Error())
+		return "", err
+	}
+	if len(n) > 2 {
+		err := fmt.Errorf("\n Ошибка введено больше двух значений: %w", errorNotTwoOperands)
+		fmt.Println(err.Error())
+		return "", err
+	}
+	var y int = 0
+	zn := "+"
+	for _, r := range n {
+		if r == "+" || r == "-" {
+			zn = r
+		} else {
+			y0, err := strconv.Atoi(r)
+			if err != nil {
+				//fmt.Printf("%T \n %v", y, y)
+				//} else {
+				err = fmt.Errorf("decompress  %w", err)
+				fmt.Println(err.Error())
+			}
+			//fmt.Printf("%T \n %v", y0, y0)
+			if zn == "+" {
+				y = y + y0
+			} else {
+				y = y - y0
+			}
+			//fmt.Printf("sum=  %v \n", y)
+			output = strconv.Itoa(y)
+
+			//fmt.Printf("output=  %v Type = %T \n", output, output)
+		}
+	}
+	return output, nil
 }
